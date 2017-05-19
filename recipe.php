1724c97a38753efda255a26f3c56533a8e4c3527
Paul Kim <root@localhost.localdomain>
2017-05-18 22:18:17 -0700

    added more nav bars

--- recipe.php
+++ recipe.php
@@ -7,56 +7,45 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <!-- css link-->
-    <link rel="stylesheet" href="css/simple-sidebar.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!------------------------css------------------------------->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/test.css">
+    <!---------------------------js----------------------------->
     <script src="js/jquery.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/3.9.0/firebase.js"></script>
+    <script src="js/firebaseScript.js"></script>
+    <script src="js/slideout.js"></script>
 </head>
 
 <body id="recipe_page">
-    <div id="wrapper">
-        <header role="banner" class="navbar navbar-fixed-top">
-            <div class="navbar-header">
-                <a href="#menu-click" id="menu-click">
-                    <div class="container" onclick="myFunction(this)">
-                        <div class="bar1"></div>
-                        <div class="bar2"></div>
-                        <div class="bar3"></div>
-                    </div>
-                </a>
-            </div>
-        </header>
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li>
-                </li>
-                <li class="sidebar-brand">
-                </li>
-                <li>
-                    <a href="index.html"><i class="fa fa-home"></i>Home</a>
-                </li>
-                <li>
-                    <a href="kitchen.html"><i class="fa fa-cutlery"></i>My Kitchen</a>
-                </li>
-                <li>
-                    <a href="qr-code-reader/index.html"><i class="fa fa-qrcode"></i>QR scanner</a>
-                </li>
-                <li>
-                    <a href="recipe.php"><i class="fa fa-archive"></i>Recipes</a>
-                </li>
-                <li>
-                    <a href="apps.html"><i class="fa fa-users"></i>Fuber Friends</a>
-                </li>
-            </ul>
-        </div>
+    <nav id="menu" class="menu">
+        <ul class="sidebar-nav">
+            <li>
+                <a href="index.html"><i class="fa fa-home"></i>Home</a>
+            </li>
+            <li>
+                <a href="ingredients.php"><i class="fa fa-cutlery"></i>My Kitchen</a>
+            </li>
+            <li>
+                <a href="qr-code-reader/index.html"><i class="fa fa-qrcode"></i>QR scanner</a>
+            </li>
+            <li>
+                <a href="kitchen.html"><i class="fa fa-archive"></i>Recipes</a>
+            </li>
+            <li>
+                <a href="apps.html"><i class="fa fa-users"></i>Fuber Friends</a>
+            </li>
+        </ul>
+    </nav>
+    <main id="panel" class="panel">
+        <button class="btn-hamburger js-slideout-toggle">
+                <div class="container" onclick="myFunction(this)">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+        </button>
         <?php
   include("Spoonacular.php");
   require_once "vendor/autoload.php";
@@ -99,7 +88,7 @@
 
   echo '</div></div></div>'
   ?>
-    </div>
+    </main>
 </body>
 <script src="js/navbar.js"></script>
 <script src="https://www.gstatic.com/firebasejs/3.9.0/firebase.js"></script>
