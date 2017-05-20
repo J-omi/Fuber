b358a631c19103ac5f6e386e6217fe2df2a43190
Paul Kim <root@localhost.localdomain>
2017-05-20 11:37:49 -0700

    added javascript method

--- js/firebaseScript.js
+++ js/firebaseScript.js
@@ -8,12 +8,25 @@ var config = {
     messagingSenderId: "221357839904"
 };
 
-firebase.initializeApp(config);
-
 $(document).ready(function(){
+    firebase.initializeApp(config);
+
     var dbRef = firebase.database().ref();
     
+    //Remove food
     $(".removeBtn").click(function(){
         dbRef.child("fridges/1/" + $(this).attr("id")).remove();
+    });
+    
+    //Add food
+    $("#submitFood").click(function(){
+        /*alert($("#foodName").val() 
+              + "\n" + $("#exp").val()
+              + "\n" + $("#qty").val()
+             );*/
+        
+        dbRef.child("fridges/1/").update($("#foodName").val());
+        dbRef.child("fridges/1/" + $("#foodName").val()).set($("#exp").val());
+        dbRef.child("fridges/1/" + $("#foodName").val()).set($("#qty").val());
     });
 });
